86be6a62763f40f2b8fe69d5906aa9760dff7a55
Yaya090807 <para.todonecesari00@gmail.com>
2023-08-24 22:38:07 -0600

    Display forecast
    Code the forecast with JS

--- appjs.js
+++ appjs.js
@@ -16,6 +16,8 @@ function changeData (response){
     iconelement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
 
     document.querySelector("#date-current").innerHTML = formatate(response.data.dt*1000);
+
+    getForecast(response.data.coord);
 }
 
 function formatate (timestamp){
@@ -73,8 +75,60 @@ function getcity(event){
     let apikey = "e8afbbe875eb43e7801438b2c0996358";
     let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${citysearched}&appid=${apikey}&units=metric`;
     axios.get(apiurl).then(changeData);
+
 }
 
+function getForecast (coordinates){
+    console.log(coordinates);
+    let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiurl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+}
+
+function displayForecast(response){
+    console.log(response.data);
+
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast-id");
+  
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
+        <div class="col-2">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> ${Math.round(
+              forecastDay.temp.day
+            )}° </span>
+            
+          </div>
+        </div>
+    `;
+      }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 let form = document.querySelector("#searcher");
 form.addEventListener("submit",getcity);
